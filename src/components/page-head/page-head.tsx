9bde36e2e76dcb807dfcfe740e430248bf0cf006
DinhHuy1405 <nguyendinhhuy14052000@gmail.com>
2024-04-26 00:04:15 +0700

    0004 update page head

--- src/components/page-head/page-head.tsx
+++ src/components/page-head/page-head.tsx
@@ -9,16 +9,16 @@ const PageHead: React.FC<{
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
             <meta name="keywords" content="portfolio, AI Engineer, blockchain, full-stack developer" />
-            <meta name="author" content="Nguyễn Lê Anh Minh" />
+            <meta name="author" content="Nguyễn Đình Huy" />
             <meta property="og:type" content="website" />
             <meta property="og:image" content="/img/favicon.png" />
             <meta
                 property="og:description"
-                content="Nguyễn Lê Anh Minh - a full-stack developer and AI Engineer. Experience in blockchain, web development, mobile development, data analyzing, AI & ML."
+                content="Nguyễn Đình Huy - a Data Engineer and AI Engineer. Experience in data analyzing, AI & ML."
             />
             <link rel="shortcut icon" href="favicon.ico"></link>
-            <title>{[title, "Nguyễn Lê Anh Minh"].filter(Boolean).join(" | ")}</title>
-            <meta name="description" content="Nguyễn Lê Anh Minh portfolio" />
+            <title>{[title, "Nguyễn Đình Huy"].filter(Boolean).join(" | ")}</title>
+            <meta name="description" content="Nguyễn Đình Huy portfolio" />
 
             {/* <!-- Favicon  --> */}
             <link rel="icon" href="/img/favicon.png" />
